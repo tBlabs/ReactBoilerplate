f5768fc043767d80ed5891db85b7a98f093169c3
tB <wendro@wp.pl>
2017-10-03 16:00:57 +0200

    axios and json-server added

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-
+import axios, { AxiosResponse } from "axios";
 import FooComponent from './components/FooComponent';
 import { Foo } from "./FooClass";
 
@@ -8,6 +8,8 @@ import { Foo } from "./FooClass";
 console.log('Start');
 
 let foo = new Foo("bar");
+
+axios.get('http://www.localhost:4444/todos/2').then((x: AxiosResponse)=>console.log(x.data));
 
 console.log(foo.toString());
     
